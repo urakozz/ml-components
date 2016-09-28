7d27d9844f0604bef1c84b24cac0f93ddc8a2e66
Yury Kozyrev <urakozz@me.com>
2016-09-28 10:15:00 +0200

    Create correlation.py

---
+++ statistics/correlation.py
@@ -0,0 +1,13 @@
+import math
+
+#Karl Pearson’s coefficient of correlation
+# np.corrcoef(a,b)[0][1]
+def corellation(A,B):
+    n = float(len(A))
+    muA = sum(A)/n
+    muB = sum(B)/n
+    diffA = map(lambda x: x - muA, A)
+    diffB = map(lambda x: x - muB, B)
+    stdA = math.sqrt((1/(n-1))* sum([d*d for d in diffA]))
+    stdB = math.sqrt((1/(n-1))* sum([d*d for d in diffB]))
+    return (sum([A[i]*B[i] for i in range(int(n))]) - n * muA * muB) / ((n-1) * stdA * stdB)
